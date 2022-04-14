41b9e3a7d2abd5a645789de94a273f08a75643c9
Mone Bell-Arrington <71570281+CrochetVandrella@users.noreply.github.com>
2022-04-13 22:57:34 -0400

    Displayed default avatar of alpaca

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ function App() {
   return (
     <div className="App">
       <div id="container">
-        <div id="background"></div>
-        <div id='accessories' className='alpaca'></div>
-        <div id='ears'  className='alpaca'></div>
-        <div id='eyes'  className='alpaca'></div>
-        <div id='hair'  className='alpaca'></div>
-        <div id='leg'   className='alpaca'></div>
-        <div id='mouth' className='alpaca'></div>
-        <div id='neck'  className='alpaca'></div>
-        <div id='nose'  className='alpaca'></div>
+
+        <div id="background" className="alpaca"></div>
+        <div id="accessories" className="alpaca"></div>
+        <div id="ears" className="alpaca"></div>
+        <div id="eyes" className="alpaca"></div>
+        <div id="hair" className="alpaca"></div>
+        <div id="leg" className="alpaca"></div>
+        <div id="mouth" className="alpaca"></div>
+        <div id="neck" className="alpaca"></div>
+        <div id="nose" className="alpaca"></div>
+
       </div>
 
     </div>
