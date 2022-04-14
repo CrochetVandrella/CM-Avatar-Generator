ac4b52ae541f5079b24b2a8a5ad306028eb5f206
Mone Bell-Arrington <71570281+CrochetVandrella@users.noreply.github.com>
2022-04-13 21:39:01 -0400

    Added avatar skeleton structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <div id="container">
+        <div id="background"></div>
+        <div id='accessories' className='alpaca'></div>
+        <div id='ears'  className='alpaca'></div>
+        <div id='eyes'  className='alpaca'></div>
+        <div id='hair'  className='alpaca'></div>
+        <div id='leg'   className='alpaca'></div>
+        <div id='mouth' className='alpaca'></div>
+        <div id='neck'  className='alpaca'></div>
+        <div id='nose'  className='alpaca'></div>
+      </div>
 
     </div>
   );
